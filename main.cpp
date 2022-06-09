03f9abaad020646a0faa83f8baebeccfaca1a209
Codeexia0 <100862101+Codeexia0@users.noreply.github.com>
2022-06-09 22:07:36 +0100

    Let's begin

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <cmath>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+int main() {
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+
+        sf::Time elapsed = clock.restart();
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        }
+
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
